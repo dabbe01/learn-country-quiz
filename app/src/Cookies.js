0e4fee701ebe3dd60a24efab7fddda23e259d9ed
rutbergphilip <philiprutberg00@gmail.com>
2021-11-11 18:49:49 +0100

    sub processors

--- app/src/Cookies.js
+++ app/src/Cookies.js
@@ -2,58 +2,75 @@ import { Link } from "wouter";
 
 export const Cookies = () => {
     return (
-        <div className="cookies">
+        <div className="wrapper">
+
             <Link href="/">Go back</Link>
-            <h1>Cookies</h1>
-            <p>
-                This site uses cookies to ensure you get the best experience on our website.
-                By using this site you agree to the use of cookies.
-            </p>
-            <p>
-                The following cookies are used:
-            </p>
-            <ul>
-                <li>
+            <div className="cookies">
+                <ul>
+                    <h1>Cookies</h1>
                     <p>
-                        <strong>CookieConsent</strong>
+                        This site uses cookies to ensure you get the best experience on our website.<br />
+                        By using this site you agree to the use of cookies.
                     </p>
                     <p>
-                        Used to identify if the user has accepted the use of cookies.
+                        The following cookies are used:
                     </p>
-                </li>
-                <li>
-                    <p>
-                        <strong>__cfduid</strong>
-                    </p>
-                    <p>
-                        Used to identify the user across sessions. (Google Analytics)
-                    </p>
-                </li>
-                <li>
-                    <p>
-                        <strong>_ga</strong>
-                    </p>
-                    <p>
-                        Used to track the usage of the website. (Google Analytics)
-                    </p>
-                </li>
-                <li>
-                    <p>
-                        <strong>_gid</strong>
-                    </p>
-                    <p>
-                        Used to track the usage of the website. (Google Analytics)
-                    </p>
-                </li>
-                <li>
-                    <p>
-                        <strong>_gat</strong>
-                    </p>
-                    <p>
-                        Used to track the usage of the website. (Google Analytics)
-                    </p>
-                </li>
-            </ul>
+                    <li>
+                        <p>
+                            <strong>CookieConsent</strong>
+                        </p>
+                        <p>
+                            Used to identify if the user has accepted the use of cookies.
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>__cfduid</strong>
+                        </p>
+                        <p>
+                            Used to identify the user across sessions. (Google Analytics)
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>_ga</strong>
+                        </p>
+                        <p>
+                            Used to track the usage of the website. (Google Analytics)
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>_gid</strong>
+                        </p>
+                        <p>
+                            Used to track the usage of the website. (Google Analytics)
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            <strong>_gat</strong>
+                        </p>
+                        <p>
+                            Used to track the usage of the website. (Google Analytics)
+                        </p>
+                    </li>
+                </ul>
+                <ul>
+                    <h1>Sub processors</h1>
+                    <li>
+                        <p>
+                            <strong>Google, Inc</strong>
+                        </p>
+                        <p>
+                            USA
+                        </p>
+                        <p>
+                            Site analytics (web) and interactions
+                        </p>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
