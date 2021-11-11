89eb115da641ebffa6b61940b7f39ecccd5334d0
rutbergphilip <philiprutberg00@gmail.com>
2021-11-11 19:12:59 +0100

    finished setup for features

--- app/src/setup.js
+++ app/src/setup.js
@@ -1,60 +1,97 @@
 import React, { useState } from 'react'
 import './setup.css'
-import { Link} from "wouter"
+import { Link } from "wouter"
 import Features from './features'
 
 function Setup() {
-	
+
 	const [selected1, setSelected1] = React.useState(localStorage.getItem('toggle-switch-1') === 'true');
 	const [selected2, setSelected2] = React.useState(localStorage.getItem('toggle-switch-2') === 'true');
 	const [selected3, setSelected3] = React.useState(localStorage.getItem('toggle-switch-3') === 'true');
-	
-	
+	const [selected4, setSelected4] = React.useState(localStorage.getItem('toggle-switch-4') === 'true');
+
+	const handleChange = (target, checked) => {
+		let storage = JSON.parse(localStorage.getItem('features'));
+
+		switch (target) {
+			case 'toggle-switch-1':
+				storage.smartScore.enabled = checked;
+				break;
+			case 'toggle-switch-2':
+				storage.randomQuestions.enabled = checked;
+				break;
+			case 'toggle-switch-3':
+				storage.tieResults.enabled = checked;
+				break;
+			case 'toggle-switch-4':
+				storage.moreStartPageFlags.enabled = checked;
+				break;
+		}
+		localStorage.setItem(target, checked);
+		localStorage.setItem('features', JSON.stringify(storage));
+	}
+
 	return (
-		
+
 		<div className="setup">
 			<div className="cointainer">
-			<label class="switch">
-			<p className="Is">Improved scoring</p>
-			<input type="checkbox" checked={selected1}
-			id='toggle-switch-1'
-        	label='This is ToggleSwitch #1'
-        	onChange={(e) => {localStorage.setItem('toggle-switch-1',`${e.target.checked}`);
-            setSelected1(e.target.checked);
-            console.log(e.target.checked)
-          }
-        }/>
-			  
- 			 <span class="slider"></span>
-		</label>
-		<label class="switch">
-		<p className="Is">Tie screen</p>	
-  			<input type="checkbox" checked={selected2}
-			id='toggle-switch-2'
-        	label='This is ToggleSwitch #2'
-        	onChange={(e) => {localStorage.setItem('toggle-switch-2',`${e.target.checked}`);
-            setSelected2(e.target.checked);
-            console.log(e.target.checked)
-          } }  /> 
- 			 <span class="slider "></span>
-			  
-		</label>
-		<label class="switch">
-		<p className="Is">Extra flags</p>
-  			<input type="checkbox" checked={selected3}
-			id='toggle-switch-3'
-        	label='This is ToggleSwitch #3'
-        	onChange={(e) => {localStorage.setItem('toggle-switch-3',`${e.target.checked}`);
-            setSelected3(e.target.checked);
-            console.log(e.target.checked)
-          } }/> 
- 			 <span class="slider"></span>
-		</label>
+				<label className="switch">
+					<p className="Is">Improved scoring</p>
+					<input type="checkbox" checked={selected1}
+						id='toggle-switch-1'
+						label='This is ToggleSwitch #1'
+						onChange={(e) => {
+							setSelected1(e.target.checked);
+							handleChange(e.target.id, e.target.checked);
+						}
+						}
+					/>
 
-			<Link to="/" className="app-link">Go to app</Link>
+					<span className="slider"></span>
+				</label>
+				<label className="switch">
+					<p className="Is">Random questions</p>
+					<input type="checkbox" checked={selected2}
+						id='toggle-switch-2'
+						label='This is ToggleSwitch #2'
+						onChange={(e) => {
+							setSelected2(e.target.checked);
+							handleChange(e.target.id, e.target.checked);
+						}
+						}
+					/>
+
+					<span className="slider"></span>
+				</label>
+				<label className="switch">
+					<p className="Is">Tie screen</p>
+					<input type="checkbox" checked={selected3}
+						id='toggle-switch-3'
+						label='This is ToggleSwitch #2'
+						onChange={(e) => {
+							setSelected3(e.target.checked);
+							handleChange(e.target.id, e.target.checked);
+						}} />
+					<span className="slider "></span>
+
+				</label>
+				<label className="switch">
+					<p className="Is">Extra flags</p>
+					<input type="checkbox" checked={selected4}
+						id='toggle-switch-4'
+						label='This is ToggleSwitch #4'
+						onChange={(e) => {
+							setSelected4(e.target.checked);
+							handleChange(e.target.id, e.target.checked);
+						}}
+					/>
+					<span className="slider"></span>
+				</label>
+
+				<Link to="/" className="app-link">Go to app</Link>
 
 			</div>
-			
+
 		</div>
 	);
 }
@@ -72,9 +109,9 @@ function Setup() {
 // 		// If error also return initialValue
 // 		console.log(error);
 // 		return initialValue;
-		
+
 // 	  }
-	
+
 // 	});
 // 	// Return a wrapped version of useState's setter function that ...
 // 	// ... persists the new value to localStorage.
@@ -94,6 +131,6 @@ function Setup() {
 // 	};
 // 	return [storedValue, setValue];
 //   }
-  
+
 export default Setup;
 
